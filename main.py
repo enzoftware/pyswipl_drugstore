b2b1570deaf9518d24428ec5ac4d151a14c00846
Enzo <lizama.enzo@gmail.com>
2019-11-23 02:59:45 +0100

    update web app with disease message

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, flash, request
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 from pyswip import Prolog
+import os
+import time
 
 # App config.
 DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
-prolog = Prolog()
-prolog.consult("console.pl")
-result = prolog.query("verify(headache)")
-print(result)
+
+
 
 class ReusableForm(Form):
     
@@ -19,18 +19,20 @@ class ReusableForm(Form):
     @app.route("/", methods=['GET', 'POST'])
     def hello():
         form = ReusableForm(request.form)
-        print (form.errors)
         if request.method == 'POST':
+
             name=request.form['name']
             fiebre = request.form.get('fiebre') == 'on'
             nausea = request.form.get('nausea') == 'on'
             diarrea = request.form.get('diarrea') == 'on'
             headache = request.form.get('headache') == 'on'
-            print (name, fiebre, nausea, diarrea, headache)
-
+            print(fiebre, nausea, diarrea, headache)
+            os.system('swipl -q -t "program" console.pl')
         if form.validate():
-            # Save the comment here.
-            flash('Thanks for registration ' + name)
+            f = open("file.txt", "r")
+            disease = f.read()
+            print(disease)
+            flash('Hola ' + name + ', por tus sintomas podemos deducir que tienes '+ disease)
         else:
             flash('Error: Debes ingresar tu nombre. ')
     
