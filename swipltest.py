ad2e7b1695e986d9574039d5d7a2672c91ebd202
Enzo <lizama.enzo@gmail.com>
2019-11-23 02:59:34 +0100

    pupdate test

--- swipltest.py
+++ swipltest.py
@@ -3,12 +3,14 @@ from pyswip import Prolog
 def main():
     prolog = Prolog()
     prolog.consult("console.pl")
-    result = prolog.query("go")
+    result = prolog.query("hypothesis(cold).")
     disease = "unknow"
     for f in result:
+        print(f)
         disease = f
     
     print(disease)
+    print(result)
 
 if __name__ == "__main__":
     main()
