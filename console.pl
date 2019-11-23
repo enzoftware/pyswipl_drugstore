60fc419b106f8789596154a7a890dbd4027e9223
Enzo <lizama.enzo@gmail.com>
2019-11-23 02:59:20 +0100

    write in file the result

--- console.pl
+++ console.pl
@@ -1,10 +1,49 @@
 /*Source code of expert system*/
 :- use_module(library('bounds')).
 
+
+coins(S, Count, Total) :-
+ 	% A=1, B=5, C=10, D=50, E=100
+	S = [A, B, C, D, E],
+	
+	Av is   1,
+	Bv is   5,
+	Cv is  10,
+	Dv is  50,
+	Ev is 100,
+	
+	Aup is Total // Av,
+	Bup is Total // Bv,
+	Cup is Total // Cv,
+	Dup is Total // Dv,
+	Eup is Total // Ev,
+	
+	A in 0..Aup,
+	B in 0..Bup,
+	C in 0..Cup,
+	D in 0..Dup,
+	E in 0..Eup,
+	
+	VA #= A*Av,
+	VB #= B*Bv,
+	VC #= C*Cv,
+	VD #= D*Dv,
+	VE #= E*Ev,
+	
+	sum(S, #=, Count),
+	VA + VB + VC + VD + VE #= Total,
+	
+	label(S).
+
+program :-
+    open('file.txt',write, Stream),
+    forall(hypothesis(Disease), write(Stream,Disease)),
+    close(Stream).
+
 go:-
-hypothesis(Disease),
-write('I believe that the patient have => '),
-write(Disease).
+%hypothesis(Disease),
+write('I believe that the patient have => cold').
+%write(Disease).
 
 % go(de):-
 %     hypothesis(Disease),
@@ -140,5 +179,4 @@ ask(Question) :-
     % ->
     % assert(yes(Question)) ;
     % assert(no(Question)), fail).
-
-% :- go.
+:- program.
